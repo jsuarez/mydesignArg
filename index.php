534a79f035e9310d9f0f72dc6206e382d430b9ed
Your Name <ivan@master.(none)>
2010-02-11 14:11:19 -0300

    ultimos cambios

--- index.php
+++ index.php
@@ -3,35 +3,74 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <meta content="Boks - 0.3.4" name="generator"/>
-    <title>Untitled</title>
+    <title>My Design</title>
     <!-- Framework CSS -->
     <link rel="stylesheet" href="css/screen.css" type="text/css" media="screen, projection"/>
     <link rel="stylesheet" href="css/print.css" type="text/css" media="print"/>
     <!--[if lt IE 8]><link rel="stylesheet" href="css/ie.css" type="text/css" media="screen, projection"/><![endif]-->
 
-    <link rel="stylesheet" href="css/style.css" type="text/css" media="print"/>
+    <link rel="stylesheet" href="css/style.css" type="text/css" />
 </head>
 
 <body>
-<div class="container showgrid">
-    <div class="span-24 last container">
+<div class="container">
+    <div class="span-24 last">
         <div class="span-22 prepend-1 append-1 last head">
             <div class="span-22 last top-head">
-                <div class="span-8"><img src="images/logo.png" alt="mydesign.com.ar"/></div>
-                <div class="span-5 prepend-4"> </div>
-                <div class="span-5 last"> </div>
+                <div class="span-7 push-1"><img src="images/logo.png" alt="mydesign.com.ar"/></div>
+                <div class="span-5 prepend-3 phones">
+                    <div class="img-phone float-left"><img src="images/icon_phone.png" alt="telefonos"/></div>
+                    <div class="number-phone">
+                        <div class="tel"><span class="italic cursive bold">Mendoza </span><span class="bold">(0261) </span><span>4237658</span></div>
+                        <div class="float-left"><span class="italic cursive bold">Bahía Blanca </span><span class="bold">(0291) </span><span>154-195047</span></div>
+                    </div>
+                </div>
+                <div class="span-4 last support"><a href="#"><span class="bold">Soporte en linea</span></a></div>
             </div>
-            <div class="clear span-22 last"> </div>
+            <div class="clear span-22 last menu">
+                <ul>
+                    <li><a href="#">Dise&ntilde;o Web</a></li>
+                    <li><a href="#">Dise&ntilde;o Gr&aacute;fico</a></li>
+                    <li><a href="#">Marketing Online</a></li>
+                    <li><a href="#">Servicios Extras</a></li>
+                    <li><a href="#">Portfolio</a></li>
+                </ul>
+            </div>
         </div><!--end .head-->
-        <div class="clear span-22 prepend-1 append-1 last">
-            <div class="span-14 prepend-1"> </div>
-            <div class="span-5 prepend-1 append-1 last"> </div>
-            <div class="clear span-22 last">
-                <div class="span-10 prepend-1"> </div>
-                <div class="span-9 prepend-1 append-1 last"> </div>
+
+        <div class="clear span-22 prepend-1 append-1 last maincontainer">
+            <div class="span-13 push-1-1 banner"> </div>
+            <div class="float-left last form-info">
+                <div class="form-top cursive"><h3>Solicitar Informaci&oacute;n</h3></div>
+                <div class="form-center">
+                    <form action="" method="post">
+                        <p>Nombre:</p>
+                        <div class="form-input">
+                            <input class="span-5" type="text" />
+                        </div>
+                        <p>Tel&eacute;fono:</p>
+                        <div class="form-input">
+                            <input class="span-5" type="text" />
+                        </div>
+                        <p>E-mail:</p>
+                        <div class="form-input">
+                            <input class="span-5" type="text" />
+                        </div>
+                        <p>Consulta:</p>
+                        <div class="form-input2">
+                            <textarea class="span-5" rows="5" cols="20"></textarea>
+                        </div>
+                    </form>
+                </div>
+                <div class="form-bottom"></div>
             </div>
-        </div>
-        <div class="clear span-22 prepend-1 append-1 last">
+            <div class="clear span-22 last content">
+                <div class="span-10 prepend-1 column1"> </div>
+                <div class="span-9 prepend-1 append-1 last column1"> </div>
+            </div>
+        </div><!--end .maincontainer-->
+
+        <div class="clear span-22 prepend-1 append-1 last footer">
             <div class="span-20 prepend-1 append-1 last"> </div>
             <div class="clear span-22 last">
                 <div class="span-3 prepend-1"> </div>
@@ -42,8 +81,8 @@
             </div>
             <div class="clear span-14 prepend-4 append-4 last"> </div>
             <div class="clear span-20 prepend-1 append-1 last"> </div>
-        </div>
-    </div><!--end .container-->
-</div>
+        </div><!--footer-->
+    </div>
+</div><!--end .container-->
 </body>
 </html>
