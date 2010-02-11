340a5e6719a3651ccf8f79c10c950cf994220627
Your Name <ivan@master.(none)>
2010-02-11 12:16:02 -0300

    primer commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta content="Boks - 0.3.4" name="generator"/>
+    <title>Untitled</title>
+    <!-- Framework CSS -->
+    <link rel="stylesheet" href="css/screen.css" type="text/css" media="screen, projection"/>
+    <link rel="stylesheet" href="css/print.css" type="text/css" media="print"/>
+    <!--[if lt IE 8]><link rel="stylesheet" href="css/ie.css" type="text/css" media="screen, projection"/><![endif]-->
+
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="print"/>
+</head>
+
+<body>
+<div class="container showgrid">
+    <div class="span-24 last container">
+        <div class="span-22 prepend-1 append-1 last head">
+            <div class="span-22 last top-head">
+                <div class="span-8"><img src="images/logo.png" alt="mydesign.com.ar"/></div>
+                <div class="span-5 prepend-4"> </div>
+                <div class="span-5 last"> </div>
+            </div>
+            <div class="clear span-22 last"> </div>
+        </div><!--end .head-->
+        <div class="clear span-22 prepend-1 append-1 last">
+            <div class="span-14 prepend-1"> </div>
+            <div class="span-5 prepend-1 append-1 last"> </div>
+            <div class="clear span-22 last">
+                <div class="span-10 prepend-1"> </div>
+                <div class="span-9 prepend-1 append-1 last"> </div>
+            </div>
+        </div>
+        <div class="clear span-22 prepend-1 append-1 last">
+            <div class="span-20 prepend-1 append-1 last"> </div>
+            <div class="clear span-22 last">
+                <div class="span-3 prepend-1"> </div>
+                <div class="span-3"> </div>
+                <div class="span-5"> </div>
+                <div class="span-4 prepend-1"> </div>
+                <div class="span-4 append-1 last"> </div>
+            </div>
+            <div class="clear span-14 prepend-4 append-4 last"> </div>
+            <div class="clear span-20 prepend-1 append-1 last"> </div>
+        </div>
+    </div><!--end .container-->
+</div>
+</body>
+</html>
