c35d4bb702f88246e18c4ac3a1c182ecabc2793f
Your Name <ivan@master.(none)>
2010-02-12 13:01:34 -0300

    ultimos cambios vie 12 feb 13:01

--- index.php
+++ index.php
@@ -1,43 +1,14 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <meta content="Boks - 0.3.4" name="generator"/>
+    <?php include("includes/head.php");?>
     <title>My Design</title>
-    <!-- Framework CSS -->
-    <link rel="stylesheet" href="css/screen.css" type="text/css" media="screen, projection"/>
-    <link rel="stylesheet" href="css/print.css" type="text/css" media="print"/>
-    <!--[if lt IE 8]><link rel="stylesheet" href="css/ie.css" type="text/css" media="screen, projection"/><![endif]-->
-
-    <link rel="stylesheet" href="css/style.css" type="text/css" />
 </head>
 
 <body>
 <div class="container">
     <div class="span-24 last">
-        <div class="span-22 prepend-1 append-1 last head">
-            <div class="span-22 last top-head">
-                <div class="span-7 push-1"><img src="images/logo.png" alt="mydesign.com.ar"/></div>
-                <div class="span-5 prepend-3 phones">
-                    <div class="img-phone float-left"><img src="images/icon_phone.png" alt="telefonos"/></div>
-                    <div class="number-phone">
-                        <div class="tel"><span class="italic cursive bold">Mendoza </span><span class="bold">(0261) </span><span>4237658</span></div>
-                        <div class="float-left"><span class="italic cursive bold">Bahía Blanca </span><span class="bold">(0291) </span><span>154-195047</span></div>
-                    </div>
-                </div>
-                <div class="span-4 last support"><a href="#"><span class="bold">Soporte en linea</span></a></div>
-            </div>
-            <div class="clear span-22 last menu">
-                <ul>
-                    <li><a href="#">Dise&ntilde;o Web</a></li>
-                    <li><a href="#">Dise&ntilde;o Gr&aacute;fico</a></li>
-                    <li><a href="#">Marketing Online</a></li>
-                    <li><a href="#">Servicios Extras</a></li>
-                    <li><a href="#">Portfolio</a></li>
-                </ul>
-            </div>
-        </div><!--end .head-->
-
+        <?php include("includes/header.php");?>
         <div class="clear span-22 prepend-1 append-1 last maincontainer">
             <div class="span-22">
                 <div class="span-13 push-1-1 banner"> </div>
@@ -114,7 +85,7 @@
                         <p><a class="bold italic" href="#">Más info</a></p>
                     </div>
 
-                    <div class="span-10 column column column-end">
+                    <div class="span-10 column column-end">
                         <div class="column-img"><img src="images/img_web6.png" alt="Web Turismo"/></div>
                         <h2>Web Turismo</h2>
                         <p>Ofrece una solución absolutamente flexible y escalable. Tanto microempresas o agentes...</p>
@@ -129,18 +100,7 @@
             </div>
         </div><!--end .maincontainer-->
         
-        <div class="clear span-22 prepend-1 append-1 last footer">
-            <div class="span-20 prepend-1 append-1 last">MyDesign © Copyright 2007 - 2010   Todos los derechos reservados   Políticas de Privacidad   Términos y Condiciones</div>
-            <div class="clear span-22 last">
-                <div class="span-3 prepend-1"> </div>
-                <div class="span-3"> </div>
-                <div class="span-5"> </div>
-                <div class="span-4 prepend-1"> </div>
-                <div class="span-4 append-1 last"> </div>
-            </div>
-            <div class="clear span-14 prepend-4 append-4 last"> </div>
-            <div class="clear span-20 prepend-1 append-1 last"> </div>
-        </div><!--footer-->
+        <?php include("includes/footer.php");?>
 
     </div>
 </div><!--end .container-->
