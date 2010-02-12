06d9951a4ed17e452781828221b84f143abdfbf1
Your Name <ivan@master.(none)>
2010-02-12 12:19:48 -0300

    nuevos cambios vie 12 feb 12:19

--- index.php
+++ index.php
@@ -39,39 +39,98 @@
         </div><!--end .head-->
 
         <div class="clear span-22 prepend-1 append-1 last maincontainer">
-            <div class="span-13 push-1-1 banner"> </div>
-            <div class="float-left last form-info">
-                <div class="form-top cursive"><h3>Solicitar Informaci&oacute;n</h3></div>
-                <div class="form-center">
-                    <form action="" method="post">
-                        <p>Nombre:</p>
-                        <div class="form-input">
-                            <input class="span-5" type="text" />
-                        </div>
-                        <p>Tel&eacute;fono:</p>
-                        <div class="form-input">
-                            <input class="span-5" type="text" />
-                        </div>
-                        <p>E-mail:</p>
-                        <div class="form-input">
-                            <input class="span-5" type="text" />
-                        </div>
-                        <p>Consulta:</p>
-                        <div class="form-input2">
-                            <textarea class="span-5" rows="5" cols="20"></textarea>
-                        </div>
-                    </form>
+            <div class="span-22">
+                <div class="span-13 push-1-1 banner"> </div>
+                <div class="float-left last form-info">
+                    <div class="form-top cursive"><h3>Solicitar Informaci&oacute;n</h3></div>
+                    <div class="form-center">
+                        <form action="" method="post">
+                            <p>Nombre:</p>
+                            <div class="form-input">
+                                <input class="span-5" type="text" />
+                            </div>
+                            <p>Tel&eacute;fono:</p>
+                            <div class="form-input">
+                                <input class="span-5" type="text" />
+                            </div>
+                            <p>E-mail:</p>
+                            <div class="form-input">
+                                <input class="span-5" type="text" />
+                            </div>
+                            <p>Consulta:</p>
+                            <div class="form-input2">
+                                <textarea class="span-5" rows="5" cols="20"></textarea>
+                            </div>
+                            <div class="button-content">
+                                <input class="button1" type="submit" value="Enviar" />
+                            </div>
+                        </form>
+                    </div>
+                    <div class="form-bottom"></div>
                 </div>
-                <div class="form-bottom"></div>
             </div>
-            <div class="clear span-22 last content">
-                <div class="span-10 prepend-1 column1"> </div>
-                <div class="span-9 prepend-1 append-1 last column1"> </div>
+            
+            <div class="span-22 last content clear">
+                <div class="span-10 prepend-1-1 column-content">
+                    <h1>Diseño Web</h1>
+                    <div class="span-10 column">
+                        <div class="column-img"><img src="images/img_web5.png" alt="Web Institucional"/></div>
+                        <h2>Web Institucional</h2>
+                        <p>Para poder insertar su Pyme o simplemente tener su pagina personal. Obteniendo un diseño profesional y permitiéndole abrir paso a su negocio en Internet.</p>
+                        <p>Consiste en realizar en forma atractiva una presentación eficaz de su empresa, de modo que la pagina sirva de toma de contacto a sus clientes a través de Internet. En ella se podrán colocar fotos, textos, imágenes y datos de contacto.</p>
+                        <p>Es la opción más usada por las PYMES, que desean tener una presencia en Internet junto con sus direcciones de correo personalizadas para reforzar su imagen ante sus clientes.</p>
+                        <p>Estamos interesados en brindar una solución para las necesidades de cada cliente. Orientado a nuevos emprendimientos y negocios jóvenes que buscan dar un paso adelante.</p>
+                        <p><a class="bold italic" href="#">Subir</a></p>
+                    </div>
+
+                    <div class="span-10 column">
+                        <div class="column-img"><img src="images/img_web2.png" alt="Web Autoadministrable"/></div>
+                        <h2>Web Autoadministrable</h2>
+                        <p>Cuenta con una interfaz para poder modificar el contenido sin causar ningún efecto en el diseño... </p>
+                        <p><a class="bold italic" href="#">Más info</a></p>
+                    </div>
+
+                    <div class="span-10 column-end">
+                        <div class="column-img"><img src="images/img_web1.png" alt="Web Imobiliaria"/></div>
+                        <h2>Web Inmobiliaria</h2>
+                        <p>A través de nuestro sistema Inmobiliario logre una mayor dinámica en la compra, venta... </p>
+                        <p><a class="bold italic" href="#">Más info</a></p>
+                    </div>
+                </div><!--end .column-content-->
+
+                <div class="span-9 last column-content">
+                    <h1>&emsp;</h1>
+                    <div class="span-10 column">
+                        <div class="column-img"><img src="images/img_web4.png" alt="Sistema catalogo de productos"/></div>
+                        <h2>Cat&aacute;logo de Productos</h2>
+                        <p>A través de nuestro sistema Inmobiliario logre una mayor dinámica en la compra, venta... </p>
+                        <p><a class="bold italic" href="#">Más info</a></p>
+                    </div>
+
+                    <div class="span-10 column">
+                        <div class="column-img"><img src="images/img_web3.png" alt="Carrito de Compras"/></div>
+                        <h2>Carrito de Compras</h2>
+                        <p>Venda sus productos a todo el mundo. Nuestra solución de comercio electrónico...  </p>
+                        <p><a class="bold italic" href="#">Más info</a></p>
+                    </div>
+
+                    <div class="span-10 column column column-end">
+                        <div class="column-img"><img src="images/img_web6.png" alt="Web Turismo"/></div>
+                        <h2>Web Turismo</h2>
+                        <p>Ofrece una solución absolutamente flexible y escalable. Tanto microempresas o agentes...</p>
+                        <p><a class="bold italic" href="#">Más info</a></p>
+                    </div>
+                </div><!--end .column-content-->
+
+            </div><!--end .content-->
+            <div class="menu2">
+                <a href="#" onmouseover="this.firstChild.src='images/button_empresa_over.png'" onmouseout="this.firstChild.src='images/button_empresa.png'"><img src="images/button_empresa.png" alt="Empresa"/> </a>
+                <a href="#" onmouseover="this.firstChild.src='images/button_blog_over.png'" onmouseout="this.firstChild.src='images/button_blog.png'"><img src="images/button_blog.png" alt="Blog"/></a>
             </div>
         </div><!--end .maincontainer-->
-
+        
         <div class="clear span-22 prepend-1 append-1 last footer">
-            <div class="span-20 prepend-1 append-1 last"> </div>
+            <div class="span-20 prepend-1 append-1 last">MyDesign © Copyright 2007 - 2010   Todos los derechos reservados   Políticas de Privacidad   Términos y Condiciones</div>
             <div class="clear span-22 last">
                 <div class="span-3 prepend-1"> </div>
                 <div class="span-3"> </div>
@@ -82,6 +141,7 @@
             <div class="clear span-14 prepend-4 append-4 last"> </div>
             <div class="clear span-20 prepend-1 append-1 last"> </div>
         </div><!--footer-->
+
     </div>
 </div><!--end .container-->
 </body>
