b56474c65318be0f3df465c6dd018453fae0f908
Your Name <ivan@master.(none)>
2010-02-17 09:25:23 -0300

    ultimos cambios

--- portfolio.php
+++ portfolio.php
@@ -11,7 +11,46 @@
         <?php include("includes/header.php");?>
         <div class="clear span-22 prepend-1 append-1 last maincontainer">
             <div class="span-22">
-                <div class="span-13 push-1-1 banner"> </div>
+                <div class="span-13 push-1-1 banner">
+                    <h1>Clientes</h1>
+                    <div class="bg-clientes-top span-13"></div>
+                    <div class="container-clientes span-13">
+                        <div class="content-clientes span-13 prepend-0">
+                            <div class="span-3">
+                                <a href="#" onmouseover="this.firstChild.src='images/img_cliente1_over.png'" onmouseout="this.firstChild.src='images/img_cliente1.png'"><img src="images/img_cliente1.png" alt="azer consultora"/></a>
+                            </div>
+                            <div class="span-3">
+                                <a href="#" onmouseover="this.firstChild.src='images/img_cliente1_over.png'" onmouseout="this.firstChild.src='images/img_cliente1.png'"><img src="images/img_cliente1.png" alt="azer consultora"/></a>
+                            </div>
+                            <div class="span-3">
+                                <a href="#" onmouseover="this.firstChild.src='images/img_cliente1_over.png'" onmouseout="this.firstChild.src='images/img_cliente1.png'"><img src="images/img_cliente1.png" alt="azer consultora"/></a>
+                            </div>
+                            <div class="span-3">
+                                <a href="#" onmouseover="this.firstChild.src='images/img_cliente1_over.png'" onmouseout="this.firstChild.src='images/img_cliente1.png'"><img src="images/img_cliente1.png" alt="azer consultora"/></a>
+                            </div>
+                        </div>
+
+                        <div class="content-clientes span-13 prepend-0">
+                            <div class="span-3">
+                                <a href="#" onmouseover="this.firstChild.src='images/img_cliente1_over.png'" onmouseout="this.firstChild.src='images/img_cliente1.png'"><img src="images/img_cliente1.png" alt="azer consultora"/></a>
+                            </div>
+                            <div class="span-3">
+                                <a href="#" onmouseover="this.firstChild.src='images/img_cliente1_over.png'" onmouseout="this.firstChild.src='images/img_cliente1.png'"><img src="images/img_cliente1.png" alt="azer consultora"/></a>
+                            </div>
+                            <div class="span-3">
+                                <a href="#" onmouseover="this.firstChild.src='images/img_cliente1_over.png'" onmouseout="this.firstChild.src='images/img_cliente1.png'"><img src="images/img_cliente1.png" alt="azer consultora"/></a>
+                            </div>
+                            <div class="span-3">
+                                <a href="#" onmouseover="this.firstChild.src='images/img_cliente1_over.png'" onmouseout="this.firstChild.src='images/img_cliente1.png'"><img src="images/img_cliente1.png" alt="azer consultora"/></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="paginador span-13">
+                        <div class="arrow-left prepend-0"><a href="#" onmouseover="this.firstChild.src='images/arrow_left_over.png'" onmouseout="this.firstChild.src='images/arrow_left.png'"><img src="images/arrow_left.png" alt="anterior"/></a></div>
+                        <div class="arrow-right append-0"><a href="#" onmouseover="this.firstChild.src='images/arrow_right_over.png'" onmouseout="this.firstChild.src='images/arrow_right.png'"><img src="images/arrow_right.png" alt="siguiente"/></a></div>
+                    </div>
+                    <div class="bg-clientes-bottom span-13"></div>
+                </div>
                 <div class="float-left last form-info">
                     <div class="form-top cursive"><h3>Solicitar Informaci&oacute;n</h3></div>
                     <div class="form-center">
@@ -44,19 +83,26 @@
             <div class="span-22 last content clear">
                 <div class="span-10 prepend-1-1 column-content">
                     <h1>Portfolio</h1>
-                    <div class="span-10 column">
-                        
+                    <div class="span-10 column-portfolio">
+                        <img src="images/img_portfolio1.jpg" alt="www.azerconsultores.com"/>
+                        <p>www.azerconsultores.com</p>
                     </div>
-
+                    <div class="span-10 column-portfolio">
+                        <img src="images/img_portfolio2.jpg" alt="www.consurpyme.com.ar"/>
+                        <p>www.consurpyme.com.ar</p>
+                    </div>
                 </div><!--end .column-content-->
 
-                <div class="span-9 last column-content">
+                <div class="span-10 last column-content">
                     <h1>&emsp;</h1>
-                    <div class="span-10 column">
-                        
+                    <div class="span-10 column-portfolio">
+                        <img src="images/img_portfolio3.jpg" alt="www.argentinaesmendoza.com.ar"/>
+                        <p>www.argentinaesmendoza.com.ar</p>
                     </div>
-
-                    
+                    <div class="span-10 column-portfolio">
+                        <img src="images/img_portfolio4.jpg" alt="img_portfolio1"/>
+                        <p>www.aikidoargentina.com</p>
+                    </div>
                 </div><!--end .column-content-->
 
             </div><!--end .content-->
